965cfbcf723a1152b7d68930124e67010fef9935
Nathan Melenbrink <nathanmelenbrink@gmail.com>
2020-07-21 21:55:33 -0400

    Update RailBot.ino

--- RailBot/RailBot.ino
+++ RailBot/RailBot.ino
@@ -1,6 +1,11 @@
 #include <ESP32Servo.h>
 #include <AccelStepper.h>
 
+// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
+const int gripperPin = 18;
+const int gripOpen = 160; // degrees to make the gripper open
+const int gripClose = 30; // degrees to make the gripper close
+
 const int ARC_PUL = A0; // define Pulse pin
 const int ARC_DIR = A1; // define Direction pin
 //const int X0_ENA = ; // define Enable pin
@@ -9,14 +14,13 @@ const int numSteppers = 3;
 const int SPEED = 400;
 const int ACCELERATION = 400;
 
-Servo myservo;
+Servo gripper;
 
 //
 bool activated[numSteppers] = {false, false, false};
 int positions[numSteppers] = {0, 0, 0};
 
 String readString;
-
 
 AccelStepper ARC(AccelStepper::DRIVER, ARC_PUL, ARC_DIR);
 AccelStepper LOC(AccelStepper::FULL4WIRE, 13, 12, 27, 33);
@@ -27,8 +31,7 @@ AccelStepper* steppers[numSteppers] = {
 };
 
 int pos = 0;    // variable to store the servo position
-// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
-int servoPin = 18;
+
 
 void setup() {
   // set all motor control pins to output
@@ -46,11 +49,8 @@ void setup() {
   ESP32PWM::allocateTimer(1);
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
-  myservo.setPeriodHertz(50);    // standard 50 hz servo
-  myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
-  // using default min/max of 1000us and 2000us
-  // different servos may require different min/max settings
-  // for an accurate 0 to 180 sweep
+  gripper.setPeriodHertz(50);    // standard 50 hz servo
+  gripper.attach(gripperPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
 }
 
 void loop() {
@@ -67,28 +67,25 @@ void loop() {
       Serial.println("Arch Elevator ACTIVE");
       deactivate();
       activated[0] = true;
-      //digitalWrite(X0_ENA, HIGH);
     }
 
     if (readString == "LOC" || readString == "loc") {
-      Serial.println("Localization ACTIVE");
+      Serial.println("Locomotion ACTIVE");
       deactivate();
       activated[1] = true;
-      //digitalWrite(X0_ENA, HIGH);
     }
 
     if (readString == "LIN" || readString == "lin") {
       Serial.println("Linear ACTIVE");
       deactivate();
       activated[2] = true;
-      //digitalWrite(X0_ENA, HIGH);
     }
 
     if (readString == "G" || readString == "grip") {
       Serial.println("Activated Grip");
       deactivate();
       // for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-      myservo.write(pos);    // tell servo to go to position in variable 'pos'
+      gripper.write(pos);    // tell servo to go to position in variable 'pos'
       //  delay(15);             // waits 15ms for the servo to reach the position
       //}
     }
@@ -97,7 +94,7 @@ void loop() {
       Serial.println("Activated Release");
       deactivate();
       //for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      myservo.write(pos);    // tell servo to go to position in variable 'pos'
+      gripper.write(pos);    // tell servo to go to position in variable 'pos'
       // delay(15);             // waits 15ms for the servo to reach the position
       //}
     }
